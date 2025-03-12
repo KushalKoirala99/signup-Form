f35304a2faaa8869f0ac404980e05e61bc24e65f
kushal <kushalkoirala58@gmail.com>
2025-03-12 15:32:49 +0545

    Feat : added a button to show the password field and add js to check if the password and confirmPassword are same, also added css for the button.

--- main.js
+++ main.js
@@ -8,4 +8,39 @@ const confirmPassword = document.querySelector('#confirmPassword')
 function validateName(){
 
     const namePattern = /^[A-Za-z]+$/;
+
+    if(!namePattern.test(firstName.value)){
+        firstName.setCustomValidity('It should only contain letters')
+        return false;
+    }
+
+    if(!namePattern.test(lastName.value)){
+        lastName.setCustomValidity('It should only contain letters')
+        return false;
+    }
+    return true;
 }
+
+
+function validatePasswordMatch(){
+    if(password.value !== confirmPassword.value){
+        confirmPassword.setCustomValidity('Passwords do not match')
+    }else{
+        confirmPassword.setCustomValidity("");
+    }
+}
+
+password.addEventListener('input',validatePasswordMatch);
+confirmPassword.addEventListener('input',validatePasswordMatch)
+
+const form = document.querySelector('form')
+form.addEventListener('submit',(e)=>{
+    if(!validateName()){
+        e.preventDefault();
+    }
+
+    validatePasswordMatch();
+    if(confirmPassword.validity.customError){
+        e.preventDefault();
+    }
+})
