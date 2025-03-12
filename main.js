1da404e5ccf7747b54173dba668adef9f6e403d5
kushal <kushalkoirala58@gmail.com>
2025-03-12 14:48:50 +0545

    Feat: Added html form validaion

---
+++ main.js
@@ -0,0 +1,11 @@
+const firstName = document.querySelector('#firstName')
+const lastName = document.querySelector('#lastName')
+const phoneNumber = document.querySelector('#phone')
+const password = document.querySelector('#password')
+const confirmPassword = document.querySelector('#confirmPassword')
+
+
+function validateName(){
+
+    const namePattern = /^[A-Za-z]+$/;
+}
